74605a203774e3ae4e0a4d03b6f31e8780d3f238
jcastro97 <jcastro@crnova.com>
2022-07-25 23:24:08 -0600

    Line chart

--- src/pages/charts/Line.jsx
+++ src/pages/charts/Line.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { LineChart, Header } from "../../components/";
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Charts" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
